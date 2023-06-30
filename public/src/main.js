4f84ccbb9b5d7fc59dd7e1cceaf9dc576e56e9ef
Doug <wanderlustking11@gmail.com>
2023-06-30 12:17:40 -0400

    Create Mobile Toggle Menu

--- public/src/main.js
+++ public/src/main.js
@@ -1,7 +1,9 @@
 window.onload = function () {
   const menu_btn = document.querySelector('.hamburger');
+  const mobile_menu = document.querySelector('.mobile-menu');
 
   menu_btn.addEventListener('click', () => {
     menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
   })
 }
