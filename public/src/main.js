ae437676c7b7f41988f42545b2dff7c3218b511b
Doug <wanderlustking11@gmail.com>
2023-06-30 13:59:06 -0400

    Home page styling;

--- public/src/main.js
+++ public/src/main.js
@@ -3,7 +3,10 @@ window.onload = function () {
   const mobile_menu = document.querySelector('.mobile-menu');
 
   menu_btn.addEventListener('click', () => {
+    const expanded = menu_btn.getAttribute('aria-expanded') === 'true' || false;
+    menu_btn.setAttribute('aria-expanded', !expanded);
+    mobile_menu.setAttribute('aria-hidden', expanded);
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
-  })
-}
+  });
+};
