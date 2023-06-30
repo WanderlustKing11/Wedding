21cb7cd2bc9153ea5196cb9fc00a1cf0ec84f855
Doug <wanderlustking11@gmail.com>
2023-06-30 11:51:38 -0400

    Create hamburger icon with JS functionality; Flex main page layout

--- public/src/main.js
+++ public/src/main.js
@@ -1,28 +1,7 @@
-const hamburgerIcon = document.getElementById('hamburger-icon');
-const menu = document.querySelector('.menu');
-let animationPlayed = false;
+window.onload = function () {
+  const menu_btn = document.querySelector('.hamburger');
 
-hamburgerIcon.addEventListener('click', () => {
-  if (!animationPlayed) {
-    menu.classList.toggle('hidden');
-    hamburgerIcon.classList.add('active');
-    hamburgerIcon.innerHTML = `<img src='/public/src/images/hamburger.gif' alt='Hamburger Icon' />`; // Replace with the path to your GIF file
-    hamburgerIcon.disabled = true;
-    hamburgerIcon.removeEventListener('click', handleClick);
-    animationPlayed = true;
-    console.log('Clicked');
-  }
-});
-
-function handleClick() {
-  hamburgerIcon.click();
+  menu_btn.addEventListener('click', () => {
+    menu_btn.classList.toggle('is-active');
+  })
 }
-
-hamburgerIcon.addEventListener('animationend', () => {
-  if (animationPlayed) {
-    hamburgerIcon.classList.remove('active');
-    hamburgerIcon.disabled = false;
-    hamburgerIcon.addEventListener('click', handleClick);
-    hamburgerIcon.innerHTML = `<img src='/public/src/images/hamburger-icon.png' alt='Hamburger Icon' />`; // Replace with the path to your image file
-  }
-});
